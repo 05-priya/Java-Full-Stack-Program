ad1bd4958066be690eaea54dc067eb2026ef14ec
05-priya <gowthamiselvaraj2002@gmail.com>
2024-07-25 13:37:55 +0530

    Assignment Completed

---
+++ JavaScript/Day2/JS_assignment-2/calci.js
@@ -0,0 +1,204 @@
+function data1(){
+    let e = document.getElementById("c_tag").innerHTML;
+    let num=document.getElementById("result").innerHTML;
+    
+        document.getElementById("result").innerHTML="0";
+}
+
+function data2(){
+    var e=document.getElementById("per_tag").innerHTML;
+    let num=document.getElementById("result").innerHTML;
+    if(num!=0){
+    document.getElementById("result").innerHTML=num+e;
+    }
+    else{
+        document.getElementById("result").innerHTML=e;
+    }
+}
+
+function data3(){
+    var e=document.getElementById("rem_tag").innerHTML;
+    let num=document.getElementById("result").innerHTML;
+    if(num!=0 && num.length!=1){
+    document.getElementById("result").innerHTML=num.slice(0,-1);
+    }
+    else{
+        document.getElementById("result").innerHTML="0";
+    }
+}
+
+function data4(){
+    var e=document.getElementById("div_tag").innerHTML;
+    let num=document.getElementById("result").innerHTML;
+    if(num!=0){
+    document.getElementById("result").innerHTML=num+e;
+    }
+    else{
+        document.getElementById("result").innerHTML="0";
+    }
+}
+
+function data5(){
+    var e=document.getElementById("sev_tag").innerHTML;
+    let num=document.getElementById("result").innerHTML;
+    if(num!=0){
+    document.getElementById("result").innerHTML=num+e;
+    }
+    else{
+        document.getElementById("result").innerHTML=e;
+    }
+}
+
+function data6(){
+    var e=document.getElementById("eig_tag").innerHTML;
+    let num=document.getElementById("result").innerHTML;
+    if(num!=0){
+    document.getElementById("result").innerHTML=num+e;
+    }
+    else{
+        document.getElementById("result").innerHTML=e;
+    }
+}
+
+function data7(){
+    var e=document.getElementById("nin_tag").innerHTML;
+    let num=document.getElementById("result").innerHTML;
+    if(num!=0){
+    document.getElementById("result").innerHTML=num+e;
+    }
+    else{
+        document.getElementById("result").innerHTML=e;
+    }
+}
+
+function data8(){
+    var e=document.getElementById("mul_tag").innerHTML;
+    let num=document.getElementById("result").innerHTML;
+    if(num!=0){
+    document.getElementById("result").innerHTML=num+e;
+    }
+    else{
+        document.getElementById("result").innerHTML="0";
+    }
+}
+function data9(){
+    var e=document.getElementById("four_tag").innerHTML;
+    let num=document.getElementById("result").innerHTML;
+    if(num!=0){
+    document.getElementById("result").innerHTML=num+e;
+    }
+    else{
+        document.getElementById("result").innerHTML=e;
+    }
+} 
+function data10(){
+    var e=document.getElementById("fiv_tag").innerHTML;
+    let num=document.getElementById("result").innerHTML;
+    if(num!=0){
+    document.getElementById("result").innerHTML=num+e;
+    }
+    else{
+        document.getElementById("result").innerHTML=e;
+    }
+}
+function data11(){
+    var e=document.getElementById("six_tag").innerHTML;
+    let num=document.getElementById("result").innerHTML;
+    if(num!=0){
+    document.getElementById("result").innerHTML=num+e;
+    }
+    else{
+        document.getElementById("result").innerHTML=e;
+    }
+}
+function data12(){
+    var e=document.getElementById("sub_tag").innerHTML;
+    let num=document.getElementById("result").innerHTML;
+    if(num!=0){
+    document.getElementById("result").innerHTML=num+e;
+    }
+    else{
+        document.getElementById("result").innerHTML="0";
+    }
+}
+function data13(){
+    var e=document.getElementById("three_tag").innerHTML;
+    let num=document.getElementById("result").innerHTML;
+    if(num!=0){
+    document.getElementById("result").innerHTML=num+e;
+    }
+    else{
+        document.getElementById("result").innerHTML=e;
+    }
+}
+function data14(){
+    var e=document.getElementById("two_tag").innerHTML;
+    let num=document.getElementById("result").innerHTML;
+    if(num!=0){
+    document.getElementById("result").innerHTML=num+e;
+    }
+    else{
+        document.getElementById("result").innerHTML=e;
+    }
+}
+function data15(){
+    var e=document.getElementById("one_tag").innerHTML;
+    let num=document.getElementById("result").innerHTML;
+    if(num!=0){
+    document.getElementById("result").innerHTML=num+e;
+    }
+    else{
+        document.getElementById("result").innerHTML=e;
+    }
+}
+function data16(){
+   
+    var e=document.getElementById("add_tag").innerHTML;
+    let num=document.getElementById("result").innerHTML;
+    if(num!=0){
+    document.getElementById("result").innerHTML=num+e;
+    }
+    else{
+        document.getElementById("result").innerHTML="0";
+    }
+}
+function data17(){
+    var e=document.getElementById("zero_tag").innerHTML;
+    let num=document.getElementById("result").innerHTML;
+    if(num!=0){
+    document.getElementById("result").innerHTML=num+e;
+    }
+    else{
+        document.getElementById("result").innerHTML=e;
+    }
+}
+function data18(){
+    var e=document.getElementById("twice_tag").innerHTML;
+    let num=document.getElementById("result").innerHTML;
+    if(num!=0){
+    document.getElementById("result").innerHTML=num+e;
+    }
+    else{
+        document.getElementById("result").innerHTML=e;
+    }
+}
+function data19(){
+    var e=document.getElementById("dot_tag").innerHTML;
+    let num=document.getElementById("result").innerHTML;
+    if(num!=0){
+    document.getElementById("result").innerHTML=num+e;
+    }
+    else{
+        document.getElementById("result").innerHTML=e;
+    }
+}
+function data20(){
+    
+    let num=document.getElementById("result").innerHTML;
+    let res=eval(num);
+    
+        document.getElementById("result").innerHTML=res;
+    
+}
+
+
